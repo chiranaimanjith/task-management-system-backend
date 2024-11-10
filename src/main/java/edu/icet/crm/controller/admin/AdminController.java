f4dceddc64cf54ecf0ee47fcfde1f9564d48fb9c
chiranaimanjith <chiranaimangeth777@gmail.com>
2024-11-10 18:06:18 +0530

    push

--- src/main/java/edu/icet/crm/controller/admin/AdminController.java
+++ src/main/java/edu/icet/crm/controller/admin/AdminController.java
@@ -3,6 +3,7 @@ package edu.icet.crm.controller.admin;
 import edu.icet.crm.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@CrossOrigin("*")
 public class AdminController {
     private final AdminService adminService;
 
