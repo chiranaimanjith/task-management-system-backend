9aba714b12bc3d644a0479c7ee0c2360a2ea4f23
chiranaimanjith <chiranaimangeth777@gmail.com>
2024-11-23 12:19:30 +0530

    add employee controller

--- src/main/java/edu/icet/crm/controller/employee/EmployeeController.java
+++ src/main/java/edu/icet/crm/controller/employee/EmployeeController.java
@@ -4,6 +4,7 @@ import edu.icet.crm.model.TaskDTO;
 import edu.icet.crm.service.employee.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
